f2a01b622e39bf52bab54036d710d67f565c7128
Ethan Kennerly <ethan@lumoslabs.com>
2016-06-24 09:57:10 -0700

    Log animation complete.

--- Toolkit/AnimationView.cs
+++ Toolkit/AnimationView.cs
@@ -5,6 +5,10 @@ namespace Finegamedesign.Utils
 {
 	public sealed class AnimationView
 	{
+		public static bool isVerbose = true;
+		private static Dictionary<GameObject, string> states = new Dictionary<GameObject, string>();
+		private static Dictionary<string, float> startTimes = new Dictionary<string, float>();
+
 		/**
 		 * Call animator.Play instead of animator.SetTrigger, in case the animator is in transition.
 		 * Test case:  2015-11-15 Enter "SAT".  Type "RAT".  Expect R selected.  Got "R" resets to unselected.
@@ -19,27 +23,104 @@ namespace Finegamedesign.Utils
 		 * Unity expects not to animate the camera or the root itself.  Instead animate the child of the root.  The root might not move.
 		 * Test case:  2016-02-13 Animate camera position.  Play.  Camera does not move.  Generate root motion curves.  Apply root motion curves.  Still camera does not move.  Assign animator to parent of camera.  Animate child.  Then camera moves.
 		 */
-		public static void SetState(GameObject gameObject, string state, bool isRestart = true)
+		public static void SetState(GameObject animatorOwner, string state, bool isRestart = true)
 		{
-			Animator animator = gameObject.GetComponent<Animator>();
+			Animator animator = animatorOwner.GetComponent<Animator>();
 			if (null != animator && animator.isInitialized)
 			{
-				// Debug.Log("AnimationView.SetState: " + gameObject + ": " + state);
+				if (isVerbose)
+				{
+					Debug.Log("AnimationView.SetState: " + animatorOwner 
+						+ ": " + state + " at " + Time.time);
+				}
 				if (isRestart)
 				{
 					animator.Play(state);
 				}
 				else
 				{
-					animator.Play(state, -1, 0f);
+					animator.Play(state);
+					// animator.Play(state, -1, 0f);
+				}
+				states[animatorOwner] = state;
+				startTimes[state] = Time.time;
+			}
+			else
+			{
+				if (null == animator) {
+					Debug.Log("AnimationView.SetState: Does animator exist? " 
+						+ animatorOwner.transform.parent 
+						+ animatorOwner + ": " + state);
+				}
+			}
+		}
+
+		/**
+		 * Return name state that was completed now, or null.
+		 * Also erases that state, so next time this is called it won't be completed now.
+		 * Expects time passed since SetState to avoid race when CompletedNow is not called before SetState in the frame.
+		 * Expects animation is on layer 0.
+		 * Syncrhonous. Does not depend on event complete animation.
+		 *
+		 * http://answers.unity3d.com/questions/351534/how-to-get-current-state-on-mecanim.html
+		 *
+		 * Blubberfish says integers will compare faster than strings.
+		 * http://answers.unity3d.com/questions/407186/how-to-get-current-state-name-on-mecanim.html
+		 */
+		public static string CompletedNow(GameObject animatorOwner, int layer = 0)
+		{
+			string completedNow = null;
+			Animator animator = animatorOwner.GetComponent<Animator>();
+			if (null != animator && animator.isInitialized)
+			{
+				AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(layer);
+				if (states.ContainsKey(animatorOwner) && null != states[animatorOwner] 
+				&& Time.time != startTimes[states[animatorOwner]]
+				&& 1.0f < info.normalizedTime
+				)
+				{
+					completedNow = states[animatorOwner];
+					if (isVerbose)
+					{
+						Debug.Log("AnimationView.CompletedNow: " + animatorOwner 
+							+ ": " + completedNow + " at " + Time.time 
+							+ " info " + info 
+							+ " IsName " + info.IsName(states[animatorOwner])
+							+ " length " + info.length
+							+ " normalizedTime " + info.normalizedTime
+						);
+						Animation animationStates = animator.GetComponent<Animation>();
+						if (null != animationStates)
+						{
+							foreach(AnimationState state in animationStates)
+							{
+								Debug.Log("    state " + state.name 
+									+ " time " + state.time 
+									+ " length " + state.length
+									+ " weight " + state.weight
+									+ " enabled " + state.enabled
+									+ " normalizedTime " + state.normalizedTime
+								);
+							}
+						}
+						AnimatorClipInfo[] clips = animator.GetCurrentAnimatorClipInfo(0);
+						foreach(AnimatorClipInfo clip in clips)
+						{
+							Debug.Log("clip " + clip.clip.name + " length " + clip.clip.length);
+						}
+					}
+					states[animatorOwner] = null;
 				}
 			}
 			else
 			{
 				if (null == animator) {
-					Debug.Log("AnimationView.SetState: Does animator exist? " + gameObject.transform.parent + gameObject + ": " + state);
+					Debug.Log("AnimationView.CompletedNow: Does animator exist? " 
+						+ animatorOwner.transform.parent 
+						+ animatorOwner);
 				}
 			}
+			return completedNow;
 		}
 	}
 }
