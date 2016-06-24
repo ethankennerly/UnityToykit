a08929618f6e5f07f585d726bfe8711cecf5cab5
Ethan Kennerly <ethan@lumoslabs.com>
2016-06-23 20:52:53 -0700

    Link key names.

--- Toolkit/KeyView.cs
+++ Toolkit/KeyView.cs
@@ -6,6 +6,10 @@ namespace Finegamedesign.Utils
 	{
 		/**
 		 * If just pressed this frame.
+		 * Key naming conventions:
+		 * http://docs.unity3d.com/Manual/ConventionalGameInput.html
+		 * List of key names:
+		 * http://answers.unity3d.com/questions/762073/c-list-of-string-name-for-inputgetkeystring-name.html
 		 */
 		public static bool IsDownNow(string keyName)
 		{
