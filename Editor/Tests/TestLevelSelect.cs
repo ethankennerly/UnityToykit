d6e4f500cbb7fcf715c739ff443090b6eae7e466
Ethan Kennerly <ethan@lumoslabs.com>
2016-08-20 20:48:48 -0700

    TestLevelSelectModel: Select 107 of 109 unlocked.

--- Editor/Tests/TestLevelSelect.cs
+++ Editor/Tests/TestLevelSelect.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Finegamedesign.Utils
 {
@@ -6,13 +7,34 @@ namespace Finegamedesign.Utils
 	public sealed class TestLevelSelectModel
 	{
 		[Test]
-		public void Select()
+		public void Select107Unlocked109()
 		{
-			LevelSelectModel model = LevelSelectModel();
+			LevelSelectModel model = new LevelSelectModel();
 			model.levelCount = 2939;
+			model.levelUnlocked = 109;
 			model.menus = new List<int>(){8, 20, 20};
+			model.Setup();
+			Assert.AreEqual(1, model.levelsPerItem[2]);
+			Assert.AreEqual(20, model.levelsPerItem[1]);
+			Assert.AreEqual(400, model.levelsPerItem[0]);
+			Assert.AreEqual(0, model.menuIndex);
 			Assert.AreEqual(false, model.Select(1));
+			Assert.AreEqual(0, model.menuIndex);
 			Assert.AreEqual(true, model.Select(0));
+			Assert.AreEqual(1, model.menuIndex);
+			Assert.AreEqual(0, model.context);
+			Assert.AreEqual(0, model.requested);
+			Assert.AreEqual(false, model.Select(6));
+			Assert.AreEqual(1, model.menuIndex);
+			Assert.AreEqual(120, model.requested);
+			Assert.AreEqual(true, model.Select(5));
+			Assert.AreEqual(2, model.menuIndex);
+			Assert.AreEqual(100, model.requested);
+			Assert.AreEqual(false, model.Select(10));
+			Assert.AreEqual(2, model.menuIndex);
+			Assert.AreEqual(true, model.Select(7));
+			Assert.AreEqual(3, model.menuIndex);
+			Assert.AreEqual(107, model.levelSelected);
 		}
 	}
 }
--- LevelSelectModel.cs
+++ LevelSelectModel.cs
@@ -2,16 +2,56 @@ using System.Collections.Generic;
 
 namespace Finegamedesign.Utils
 {
+	// Nested level select.
+	// Example:  WordBrain.
 	public sealed class LevelSelectModel
 	{
+		public int context = 0;
 		public int levelCount = -1;
 		public int levelUnlocked = 0;
-		public int menus = new List<int>();
+		public int levelSelected = 0;
+		public int requested = 0;
+		public List<int> menus = new List<int>();
+		public List<int> menuSelected = new List<int>();
+		public List<int> levelsPerItem = new List<int>();
+		public int menuIndex = 0;
+
+		public void Setup()
+		{
+			for (int index = 0; index < DataUtil.Length(menus); index++)
+			{
+				int menu = menus[index];
+				levelsPerItem.Add(1);
+				for (int previous = 0; previous < index; previous++)
+				{
+					levelsPerItem[previous] *= menu;
+				}
+				menuSelected.Add(-1);
+			}
+		}
 
 		// Item in menu.
 		// Return true if available.
-		public bool Select(int index)
+		public bool Select(int itemIndex)
 		{
+			requested = context;
+			if (menuIndex < DataUtil.Length(menus) - 1)
+			{
+				requested += itemIndex * levelsPerItem[menuIndex];
+			}
+			else
+			{
+				requested += itemIndex;
+			}
+			bool isUnlocked = requested <= levelUnlocked;
+			if (isUnlocked)
+			{
+				levelSelected = requested;
+				context = requested;
+				menuSelected[menuIndex] = itemIndex;
+				menuIndex++;
+			}
+			return isUnlocked;
 		}
 	}
 }
