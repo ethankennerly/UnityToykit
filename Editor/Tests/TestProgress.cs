2065b4ac7a29dde7b2c6862670bb750c0d342667
Ethan Kennerly <ethan@lumoslabs.com>
2016-09-18 11:02:23 -0700

    Progress: Pop index.

--- Editor/Tests/TestProgress.cs
+++ Editor/Tests/TestProgress.cs
@@ -1,143 +1,160 @@
-/**
- * This script must be in an Editor folder.
- * Test case:  2014-01 JimboFBX expects "using NUnit.Framework;"
- * Got "The type or namespace 'NUnit' could not be found."
- * http://answers.unity3d.com/questions/610988/unit-testing-unity-test-tools-v10-namespace-nunit.html
- */
-using System.Collections.Generic;  // List
-using UnityEngine;
+using System.Collections.Generic/*<List>*/;
 using NUnit.Framework;
 
-[TestFixture]
-internal class TestProgress
+namespace Finegamedesign.Utils
 {
-	[Test]
-	public void CreepUp()
+	public sealed class TestProgress
 	{
-		Progress progress = new Progress();
-		progress.radius = 0.25f;
-		Assert.AreEqual(0.0f, progress.Creep(0.0f / 100));
-		Assert.AreEqual(0.0f, progress.Creep(50.0f / 100));
-		Assert.AreEqual(0.25f, progress.Creep(100.0f / 100));
-		Assert.AreEqual(0.25f + 3.0f / 16.0f, 
-			progress.Creep(100.0f / 100));
-	}
+		[Test]
+		public void CreepUp()
+		{
+			Progress progress = new Progress();
+			progress.radius = 0.25f;
+			Assert.AreEqual(0.0f, progress.Creep(0.0f / 100));
+			Assert.AreEqual(0.0f, progress.Creep(50.0f / 100));
+			Assert.AreEqual(0.25f, progress.Creep(100.0f / 100));
+			Assert.AreEqual(0.25f + 3.0f / 16.0f, 
+				progress.Creep(100.0f / 100));
+		}
 
-	[Test]
-	public void CreepDown()
-	{
-		Progress progress = new Progress();
-		progress.radius = 0.25f;
-		Assert.AreEqual(0.0f, progress.Creep(0 / 100.0f));
-		Assert.AreEqual(0.0f, progress.Creep(50 / 100.0f));
-		Assert.AreEqual(0.25f, progress.Creep(100 / 100.0f));
-		Assert.AreEqual(0.25f - 3.0f / 16.0f, progress.Creep(0 / 100.0f));
-		Assert.AreEqual(0.0f, progress.Creep(0 / 100.0f));
-		Assert.AreEqual(0.0f, progress.Creep(0 / 100.0f));
-		Assert.AreEqual(0.25f, progress.Creep(100 / 100.0f));
-	}
+		[Test]
+		public void CreepDown()
+		{
+			Progress progress = new Progress();
+			progress.radius = 0.25f;
+			Assert.AreEqual(0.0f, progress.Creep(0 / 100.0f));
+			Assert.AreEqual(0.0f, progress.Creep(50 / 100.0f));
+			Assert.AreEqual(0.25f, progress.Creep(100 / 100.0f));
+			Assert.AreEqual(0.25f - 3.0f / 16.0f, progress.Creep(0 / 100.0f));
+			Assert.AreEqual(0.0f, progress.Creep(0 / 100.0f));
+			Assert.AreEqual(0.0f, progress.Creep(0 / 100.0f));
+			Assert.AreEqual(0.25f, progress.Creep(100 / 100.0f));
+		}
 
-	[Test]
-	public void CreepCheckpoint()
-	{
-		Progress progress = new Progress();
-		progress.radius = 0.25f;
-		progress.SetCheckpointStep(0.125f);
-		Assert.AreEqual(0.125f, progress.Creep(100 / 100.0f),
-			"checkpoint " + progress.checkpoint.ToString());
-		Assert.AreEqual(true, progress.isCheckpoint);
-		progress.UpdateCheckpoint();
-		progress.Creep(52 / 100.0f);
-		Assert.AreEqual(false, progress.isCheckpoint);
-		progress.normal = 0.242f;
-		Assert.AreEqual(0.25f, progress.Creep(100 / 100.0f));
-		Assert.AreEqual(true, progress.isCheckpoint);
-		progress.UpdateCheckpoint();
-		progress.normal = 0.362f;
-		Assert.AreEqual(0.375, progress.Creep(100 / 100.0f));
-		Assert.AreEqual(true, progress.isCheckpoint);
-		progress.UpdateCheckpoint();
-		progress.SetLevelNormal(0);
-		Assert.AreEqual(0.125f, progress.Creep(100 / 100.0f),
-			"checkpoint " + progress.checkpoint.ToString());
-		progress.UpdateCheckpoint();
-		progress.SetLevelNormal(740);
-		Assert.AreEqual(0.75f, progress.Creep(100 / 100.0f));
-	}
+		[Test]
+		public void CreepCheckpoint()
+		{
+			Progress progress = new Progress();
+			progress.radius = 0.25f;
+			progress.SetCheckpointStep(0.125f);
+			Assert.AreEqual(0.125f, progress.Creep(100 / 100.0f),
+				"checkpoint " + progress.checkpoint.ToString());
+			Assert.AreEqual(true, progress.isCheckpoint);
+			progress.UpdateCheckpoint();
+			progress.Creep(52 / 100.0f);
+			Assert.AreEqual(false, progress.isCheckpoint);
+			progress.normal = 0.242f;
+			Assert.AreEqual(0.25f, progress.Creep(100 / 100.0f));
+			Assert.AreEqual(true, progress.isCheckpoint);
+			progress.UpdateCheckpoint();
+			progress.normal = 0.362f;
+			Assert.AreEqual(0.375, progress.Creep(100 / 100.0f));
+			Assert.AreEqual(true, progress.isCheckpoint);
+			progress.UpdateCheckpoint();
+			progress.SetLevelNormal(0);
+			Assert.AreEqual(0.125f, progress.Creep(100 / 100.0f),
+				"checkpoint " + progress.checkpoint.ToString());
+			progress.UpdateCheckpoint();
+			progress.SetLevelNormal(740);
+			Assert.AreEqual(0.75f, progress.Creep(100 / 100.0f));
+		}
 
-	[Test]
-	public void Pop()
-	{
-		List<int> cards = new List<int>{10, 11, 12, 13};
-		Progress progress = new Progress();
-		progress.normal = 0.5f;
-		Assert.AreEqual(12, progress.Pop(cards));
-		Assert.AreEqual(3, progress.level);
-		Assert.AreEqual(4, progress.levelMax);
-		Assert.AreEqual(11, progress.Pop(cards));
-		Assert.AreEqual(2, progress.level);
-		Assert.AreEqual(4, progress.levelMax);
-		Assert.AreEqual(13, progress.Pop(cards));
-		Assert.AreEqual(4, progress.level);
-		Assert.AreEqual(4, progress.levelMax);
-		progress.normal = 1.0f;
-		Assert.AreEqual(10, progress.Pop(cards));
-		Assert.AreEqual(1, progress.level);
-		Assert.AreEqual(4, progress.levelMax);
-	}
+		[Test]
+		public void PopIndex()
+		{
+			List<int> cards = new List<int>{10, 11, 12, 13};
+			Progress progress = new Progress();
+			for (int round = 0; round < 4; round++)
+			{
+				progress.normal = 0.5f;
+				progress.SetupIndexes(DataUtil.Length(cards), 1);
+				Assert.AreEqual(2, progress.PopIndex());
+				Assert.AreEqual(2, progress.level);
+				Assert.AreEqual(4, progress.levelMax);
+				Assert.AreEqual(3, progress.PopIndex());
+				Assert.AreEqual(3, progress.level);
+				Assert.AreEqual(4, progress.levelMax);
+				progress.normal = 1.0f;
+				Assert.AreEqual(1, progress.PopIndex());
+				Assert.AreEqual(1, progress.level);
+				Assert.AreEqual(4, progress.levelMax);
+			}
+		}
 
-	[Test]
-	public void GetLevelNormal()
-	{
-		Progress progress = new Progress();
-		progress.levelNormalMax = 1000;
-		Assert.AreEqual(0, progress.GetLevelNormal());
-		progress.levelMax = 2000;
-		progress.normal = 0.5f;
-		Assert.AreEqual(500, progress.GetLevelNormal());
-		progress.normal = 0.75f;
-		Assert.AreEqual(750, progress.GetLevelNormal());
-		progress.levelMax = 4000;
-		Assert.AreEqual(750, progress.GetLevelNormal());
-	}
+		[Test]
+		public void Pop()
+		{
+			List<int> cards = new List<int>{10, 11, 12, 13};
+			Progress progress = new Progress();
+			progress.normal = 0.5f;
+			Assert.AreEqual(12, progress.Pop(cards));
+			Assert.AreEqual(3, progress.level);
+			Assert.AreEqual(4, progress.levelMax);
+			Assert.AreEqual(11, progress.Pop(cards));
+			Assert.AreEqual(2, progress.level);
+			Assert.AreEqual(4, progress.levelMax);
+			Assert.AreEqual(13, progress.Pop(cards));
+			Assert.AreEqual(4, progress.level);
+			Assert.AreEqual(4, progress.levelMax);
+			progress.normal = 1.0f;
+			Assert.AreEqual(10, progress.Pop(cards));
+			Assert.AreEqual(1, progress.level);
+			Assert.AreEqual(4, progress.levelMax);
+		}
 
-	[Test]
-	public void SetLevelNormal()
-	{
-		Progress progress = new Progress();
-		progress.levelNormalMax = 1000;
-		Assert.AreEqual(0.0f, progress.normal);
-		Assert.AreEqual(0, progress.GetLevelNormal());
-		progress.SetLevelNormal(500);
-		Assert.AreEqual(0.5f, progress.normal);
-		Assert.AreEqual(500, progress.GetLevelNormal());
-		progress.levelMax = 2000;
-		progress.SetLevelNormal(500);
-		Assert.AreEqual(0.5f, progress.normal);
-		Assert.AreEqual(500, progress.GetLevelNormal());
-		progress.SetLevelNormal(750);
-		Assert.AreEqual(0.75f, progress.normal);
-		Assert.AreEqual(750, progress.GetLevelNormal());
-		progress.levelMax = 4000;
-		progress.SetLevelNormal(375);
-		Assert.AreEqual(0.375f, progress.normal);
-		Assert.AreEqual(375, progress.GetLevelNormal());
-		progress.SetLevelNormal(0);
-		Assert.AreEqual(0.0f, progress.normal);
-		Assert.AreEqual(0, progress.GetLevelNormal());
-	}
+		[Test]
+		public void GetLevelNormal()
+		{
+			Progress progress = new Progress();
+			progress.levelNormalMax = 1000;
+			Assert.AreEqual(0, progress.GetLevelNormal());
+			progress.levelMax = 2000;
+			progress.normal = 0.5f;
+			Assert.AreEqual(500, progress.GetLevelNormal());
+			progress.normal = 0.75f;
+			Assert.AreEqual(750, progress.GetLevelNormal());
+			progress.levelMax = 4000;
+			Assert.AreEqual(750, progress.GetLevelNormal());
+		}
 
-	[Test]
-	public void SetLevelNormalUnlocked()
-	{
-		Progress progress = new Progress();
-		progress.levelNormalMax = 10000;
-		progress.levelMax = 3000;
-		Assert.AreEqual(0.0f, progress.normal);
-		Assert.AreEqual(0, progress.GetLevelNormal());
-		progress.SetLevelNormalUnlocked(5000);
-		Assert.AreEqual(1500, progress.levelUnlocked);
-		progress.SetLevelNormalUnlocked(249);
-		Assert.AreEqual(74, progress.levelUnlocked);
+		[Test]
+		public void SetLevelNormal()
+		{
+			Progress progress = new Progress();
+			progress.levelNormalMax = 1000;
+			Assert.AreEqual(0.0f, progress.normal);
+			Assert.AreEqual(0, progress.GetLevelNormal());
+			progress.SetLevelNormal(500);
+			Assert.AreEqual(0.5f, progress.normal);
+			Assert.AreEqual(500, progress.GetLevelNormal());
+			progress.levelMax = 2000;
+			progress.SetLevelNormal(500);
+			Assert.AreEqual(0.5f, progress.normal);
+			Assert.AreEqual(500, progress.GetLevelNormal());
+			progress.SetLevelNormal(750);
+			Assert.AreEqual(0.75f, progress.normal);
+			Assert.AreEqual(750, progress.GetLevelNormal());
+			progress.levelMax = 4000;
+			progress.SetLevelNormal(375);
+			Assert.AreEqual(0.375f, progress.normal);
+			Assert.AreEqual(375, progress.GetLevelNormal());
+			progress.SetLevelNormal(0);
+			Assert.AreEqual(0.0f, progress.normal);
+			Assert.AreEqual(0, progress.GetLevelNormal());
+		}
+
+		[Test]
+		public void SetLevelNormalUnlocked()
+		{
+			Progress progress = new Progress();
+			progress.levelNormalMax = 10000;
+			progress.levelMax = 3000;
+			Assert.AreEqual(0.0f, progress.normal);
+			Assert.AreEqual(0, progress.GetLevelNormal());
+			progress.SetLevelNormalUnlocked(5000);
+			Assert.AreEqual(1500, progress.levelUnlocked);
+			progress.SetLevelNormalUnlocked(249);
+			Assert.AreEqual(74, progress.levelUnlocked);
+		}
 	}
 }
--- Progress.cs
+++ Progress.cs
@@ -1,153 +1,189 @@
 using UnityEngine/*<Mathf>*/;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 
-
-// Requirements:
-// 
-// 	May go down in level.
-// 	Do not repeat levels.  Once a level was repeated.
-// 
-// System:
-// 
-// 	Adjust normalized level range.
-// 	Deck of cards.
-// 		Draw from deck at normalized position.
-// 			Original index.
-// 			Closest index in remaining.
-// 		Identify original index.
-//
-// Test case:  2016-05-21 Anagram Attack.  Very high level.
-// Jennifer Russ expects to not feel overwhelmed.
-// Jennifer Russ expects to not repeat a word.
-public class Progress
+namespace Finegamedesign.Utils
 {
-	public int level = 0;
-	public int levelNormalMax = 1000;
-	public int levelMax = 0;
-	public int levelUnlocked = 0;
-	public float normal = 0.0f;
-	public float radius = 
-			0.03125f;
-			// 0.0625f;
-			// 0.1f;
-	private ArrayList cardsOriginally;
-	public bool isVerbose = false;
-
-	public bool isCheckpoint = false;
-	public float checkpointStep = -1.0f;
-	public float checkpoint = -1.0f;
-
-	// Pop interpolated card from 0 to 1.
-	// Example:  Editor/Tests/TestProgress.cs
-	public T Pop<T>(List<T> cards, int min = 0)
- 	{
-		int index = (int)Mathf.Floor(normal * cards.Count);
-		index = Mathf.Min(cards.Count - 1, index);
-		index = Mathf.Max(min, index);
-		T card = cards[index];
-		if (null == cardsOriginally) {
-			cardsOriginally = new ArrayList(cards);
-			levelMax = cards.Count;
-		}
-		level = cardsOriginally.IndexOf(card) + 1;
-		cards.RemoveAt(index);
-		return card;
-	}
-
-	public float NextCreep(float performanceNormal)
-	{
-		float change = (performanceNormal - 0.5f) * 2.0f;
-		change = Mathf.Max(-1.0f, Mathf.Min(1.0f, change));
-		float progress = change * radius;
-		float remaining = 1.0f - normal;
-		progress *= remaining;
-		float next = normal + progress;
-		next = Mathf.Max(0.0f, Mathf.Min(1.0f, next));
-		return next;
-	}
-
-	// Midway is no change.
-	// Example:  Editor/Tests/TestProgress.cs
-	public float Creep(float performanceNormal)
-	{
-		normal = NextCreep(performanceNormal);
-		normal = ClampCheckpoint(normal);
-		if (isVerbose) {
-			DebugUtil.Log("Progress.creep: normal " + normal + " performance " + performanceNormal);
-		}
-		return normal;
-	}
-
-	//
-	// At each step of progress, clamp to checkpoint.
-	// Example:  Editor/Tests/TestProgress.cs
+	// Requirements:
 	// 
-	public void SetCheckpointStep(float step)
+	// 	May go down in level.
+	// 	Do not repeat levels.  Once a level was repeated.
+	// 
+	// System:
+	// 
+	// 	Adjust normalized level range.
+	// 	Deck of cards.
+	// 		Draw from deck at normalized position.
+	// 			Original index.
+	// 			Closest index in remaining.
+	// 		Identify original index.
+	//
+	// Test case:  2016-05-21 Anagram Attack.  Very high level.
+	// Jennifer Russ expects to not feel overwhelmed.
+	// Jennifer Russ expects to not repeat a word.
+	public sealed class Progress
 	{
-		checkpointStep = step;
-		if (0.0f <= step) {
-			checkpoint = Mathf.Floor(normal / checkpointStep + 1) * checkpointStep;
+		public int level = 0;
+		public int levelNormalMax = 1000;
+		public int levelMax = 0;
+		public int levelUnlocked = 0;
+		public float normal = 0.0f;
+		public float radius = 
+				0.03125f;
+				// 0.0625f;
+				// 0.1f;
+		private ArrayList cardsOriginally;
+		public bool isVerbose = false;
+
+		public bool isCheckpoint = false;
+		public float checkpointStep = -1.0f;
+		public float checkpoint = -1.0f;
+
+		private List<int> indexes = new List<int>();
+		private int indexesLength = -1;
+		private int indexesStart = 0;
+
+		// Example:  Editor/Tests/TestProgress.cs
+		public void SetupIndexes(int length, int start = 0)
+		{
+			indexesLength = length;
+			indexesStart = start;
+			DataUtil.Clear(indexes);
+			for (int index = start; index < length; index++)
+			{
+				indexes.Add(index);
+			}
+			levelMax = length;
+		}
+
+		// Pop interpolated card from 0 to 1.
+		// Example:  Editor/Tests/TestProgress.cs
+		public int PopIndex()
+		{
+			if (DataUtil.Length(indexes) <= 0)
+			{
+				SetupIndexes(indexesLength, indexesStart);
+			}
+			int length = DataUtil.Length(indexes);
+			int interpolated = (int)Mathf.Floor(normal * length);
+			interpolated = Mathf.Min(length - 1, interpolated);
+			int index = indexes[interpolated];
+			indexes.RemoveAt(interpolated);
+			level = index;
+			return index;
+		}
+
+		// Pop interpolated card from 0 to 1.
+		// Example:  Editor/Tests/TestProgress.cs
+		public T Pop<T>(List<T> cards, int min = 0)
+		{
+			int index = (int)Mathf.Floor(normal * cards.Count);
+			index = Mathf.Min(cards.Count - 1, index);
+			index = Mathf.Max(min, index);
+			T card = cards[index];
+			if (null == cardsOriginally) {
+				cardsOriginally = new ArrayList(cards);
+				levelMax = cards.Count;
+			}
+			level = cardsOriginally.IndexOf(card) + 1;
+			cards.RemoveAt(index);
+			return card;
+		}
+
+		public float NextCreep(float performanceNormal)
+		{
+			float change = (performanceNormal - 0.5f) * 2.0f;
+			change = Mathf.Max(-1.0f, Mathf.Min(1.0f, change));
+			float progress = change * radius;
+			float remaining = 1.0f - normal;
+			progress *= remaining;
+			float next = normal + progress;
+			next = Mathf.Max(0.0f, Mathf.Min(1.0f, next));
+			return next;
+		}
+
+		// Midway is no change.
+		// Example:  Editor/Tests/TestProgress.cs
+		public float Creep(float performanceNormal)
+		{
+			normal = NextCreep(performanceNormal);
+			normal = ClampCheckpoint(normal);
 			if (isVerbose) {
-				DebugUtil.Log("Progress.SetCheckpointStep: checkpoint " + checkpoint 
-					+ " normal " + normal + " level " + GetLevelNormal());
+				DebugUtil.Log("Progress.creep: normal " + normal + " performance " + performanceNormal);
+			}
+			return normal;
+		}
+
+		//
+		// At each step of progress, clamp to checkpoint.
+		// Example:  Editor/Tests/TestProgress.cs
+		// 
+		public void SetCheckpointStep(float step)
+		{
+			checkpointStep = step;
+			if (0.0f <= step) {
+				checkpoint = Mathf.Floor(normal / checkpointStep + 1) * checkpointStep;
+				if (isVerbose) {
+					DebugUtil.Log("Progress.SetCheckpointStep: checkpoint " + checkpoint 
+						+ " normal " + normal + " level " + GetLevelNormal());
+				}
+			}
+			else {
+				checkpoint = 0.0f;
 			}
 		}
-		else {
-			checkpoint = 0.0f;
-		}
-	}
 
-	public float ClampCheckpoint(float normal)
-	{
-		isCheckpoint = 0.0f <= checkpoint && checkpoint <= normal;
-		if (isCheckpoint) {
-			normal = checkpoint;
+		public float ClampCheckpoint(float normal)
+		{
+			isCheckpoint = 0.0f <= checkpoint && checkpoint <= normal;
+			if (isCheckpoint) {
+				normal = checkpoint;
+			}
+			return normal;
 		}
-		return normal;
-	}
 
-	//
-	// Discards excess progress after checkpoint.
-	// Example:  Editor/Tests/TestProgress.cs
-	// 
-	public float UpdateCheckpoint(float normal = -1.0f)
-	{
-		if (normal == -1.0f) {
-			normal = this.normal;
+		//
+		// Discards excess progress after checkpoint.
+		// Example:  Editor/Tests/TestProgress.cs
+		// 
+		public float UpdateCheckpoint(float normal = -1.0f)
+		{
+			if (normal == -1.0f) {
+				normal = this.normal;
+			}
+			normal = ClampCheckpoint(normal);
+			if (isCheckpoint) {
+				normal = checkpoint;
+				SetCheckpointStep(checkpointStep);
+			}
+			return normal;
 		}
-		normal = ClampCheckpoint(normal);
-		if (isCheckpoint) {
-			normal = checkpoint;
+
+		// Linear interpolation for normalized level.
+		// Invariant when adding or removing levels.
+		// Example:  Editor/Tests/TestProgress.cs
+		public int GetLevelNormal()
+		{
+			float levelRate = (float)(levelMax != 0 ? levelMax : 1.0f);
+			level = (int)(Mathf.Ceil(normal * levelMax));
+			return (int)(Mathf.Ceil(levelNormalMax * level / levelRate));
+		}
+
+		public void SetLevelNormal(int levelNormal)
+		{
+			normal = levelNormal / (float)levelNormalMax;
+			if (0 == levelMax)
+			{
+				levelMax = levelNormalMax;
+			}
 			SetCheckpointStep(checkpointStep);
 		}
-		return normal;
-	}
 
-	// Linear interpolation for normalized level.
-	// Invariant when adding or removing levels.
-	// Example:  Editor/Tests/TestProgress.cs
-	public int GetLevelNormal()
-	{
-		float levelRate = (float)(levelMax != 0 ? levelMax : 1.0f);
-		level = (int)(Mathf.Ceil(normal * levelMax));
-		return (int)(Mathf.Ceil(levelNormalMax * level / levelRate));
-	}
-
-	public void SetLevelNormal(int levelNormal)
-	{
-		normal = levelNormal / (float)levelNormalMax;
-		if (0 == levelMax)
+		// Example:  Editor/Tests/TestProgress.cs
+		public void SetLevelNormalUnlocked(int levelNormal)
 		{
-			levelMax = levelNormalMax;
+			SetLevelNormal(levelNormal);
+			levelUnlocked = (int)(normal * levelMax);
 		}
-		SetCheckpointStep(checkpointStep);
-	}
-
-	// Example:  Editor/Tests/TestProgress.cs
-	public void SetLevelNormalUnlocked(int levelNormal)
-	{
-		SetLevelNormal(levelNormal);
-		levelUnlocked = (int)(normal * levelMax);
 	}
 }
