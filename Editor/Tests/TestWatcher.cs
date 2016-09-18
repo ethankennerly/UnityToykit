680529c10ba5441d7635fb09003158e34cdea77f
Ethan Kennerly <ethan@lumoslabs.com>
2016-09-17 17:04:52 -0700

    Watcher: Detect when a variable changes.

---
+++ Editor/Tests/TestWatcher.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace Finegamedesign.Utils
+{
+	public sealed class TestWatcher
+	{
+		[Test]
+		public void UpdateIsChangeTo()
+		{
+			bool isInMenu = true;
+			var isInMenuWatcher = WatcherFactory.Create(isInMenu);
+			isInMenuWatcher.Update(isInMenu);
+			Assert.AreEqual(false, isInMenuWatcher.IsChangeTo(false));
+			Assert.AreEqual(false, isInMenuWatcher.IsChange());
+			isInMenu = false;
+			Assert.AreEqual(false, isInMenuWatcher.IsChangeTo(false));
+			Assert.AreEqual(false, isInMenuWatcher.IsChange());
+			isInMenuWatcher.Update(isInMenu);
+			Assert.AreEqual(true, isInMenuWatcher.IsChangeTo(false));
+			Assert.AreEqual(true, isInMenuWatcher.IsChange());
+			Assert.AreEqual(false, isInMenuWatcher.IsChangeTo(true));
+			isInMenuWatcher.Setup(isInMenu);
+			Assert.AreEqual(false, isInMenuWatcher.IsChangeTo(false));
+			Assert.AreEqual(false, isInMenuWatcher.IsChange());
+			Assert.AreEqual(false, isInMenuWatcher.IsChangeTo(true));
+		}
+	}
+}
---
+++ Watcher.cs
@@ -0,0 +1,65 @@
+using System/*<IComparable>*/;
+
+namespace Finegamedesign.Utils
+{
+	// Useful to detect when a value has changed, and when it has changed to an exact value.
+	// Can watch external data structures.
+	// Can also be used to store the state itself.
+	// Example: Editor/Tests/TestWatcher.cs
+	public sealed class Watcher<T> where T : IComparable
+	{
+		public static Watcher<T> Create(T subjectValue)
+		{
+			Watcher<T> watch = new Watcher<T>();
+			watch.Setup(subjectValue);
+			return watch;
+		}
+
+		public T current;
+		public T previous;
+
+		public void Setup(T subjectValue)
+		{
+			current = subjectValue;
+			previous = subjectValue;
+		}
+
+		public void Update(T subjectValue)
+		{
+			previous = current;
+			current = subjectValue;
+		}
+
+		public bool IsChange()
+		{
+			return !IsEqual(current, previous);
+		}
+
+		public bool IsChangeTo(T subjectValue)
+		{
+			return IsChange() && IsEqual(current, subjectValue);
+		}
+
+		// Work around comparison of values for C# generics:
+		// http://stackoverflow.com/questions/6480577/how-to-compare-values-of-generic-types
+		private bool IsEqual(T a, T b)
+		{
+			return 0 == a.CompareTo(b);
+		}
+	}
+
+	// Refer to Watcher without generic syntax for needed types:  bool, string.
+	public sealed class WatcherFactory
+	{
+		public static Watcher<bool> Create(bool subjectValue)
+		{
+			return Watcher<bool>.Create(subjectValue);
+		}
+
+		public static Watcher<string> Create(string subjectValue)
+		{
+			return Watcher<string>.Create(subjectValue);
+		}
+
+	}
+}
