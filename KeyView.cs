fad3b67fc4faca1ea2b2f129d3be9c1d2a3e7f1c
Ethan Kennerly <ethan@lumoslabs.com>
2016-10-09 21:05:04 -0700

    Key View: Wrap input string.

--- KeyView.cs
+++ KeyView.cs
@@ -4,16 +4,19 @@ namespace Finegamedesign.Utils
 {
 	public sealed class KeyView
 	{
-		//
 		// If just pressed this frame.
 		// Key naming conventions:
 		// http://docs.unity3d.com/Manual/ConventionalGameInput.html
 		// List of key names:
 		// http://answers.unity3d.com/questions/762073/c-list-of-string-name-for-inputgetkeystring-name.html
-		// 
 		public static bool IsDownNow(string keyName)
 		{
 			return Input.GetKeyDown(keyName);
+		}
+
+		public static string InputString()
+		{
+			return Input.inputString;
 		}
 	}
 }
