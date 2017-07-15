e626bf8afb3f35ca705ecd5d9c0af6be3803a909
Ethan Kennerly <ethan@lumoslabs.com>
2017-07-15 12:27:45 -0700

    Key View: Lists backspace and return characters.

--- KeyView.cs
+++ KeyView.cs
@@ -1,9 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Finegamedesign.Utils
 {
 	public sealed class KeyView
 	{
+		public static bool isVerbose = false;
+		public static string backspaceCharacter = "\b";
+		public static string newlineCharacter = "\n";
+
 		// If just pressed this frame.
 		// Key naming conventions:
 		// http://docs.unity3d.com/Manual/ConventionalGameInput.html
@@ -14,9 +19,31 @@ namespace Finegamedesign.Utils
 			return Input.GetKeyDown(keyName);
 		}
 
+		// Represents enter key by "\n"
+		// Represents delete key or backspace key by "\b"
+		// Unity inserts backspace character already but not delete key.
 		public static string InputString()
 		{
-			return Input.inputString;
+			string input = Input.inputString;
+			if (Input.GetKeyDown("delete"))
+			{
+				input += backspaceCharacter;
+			}
+			if (Input.GetKeyDown("return"))
+			{
+				input += newlineCharacter;
+			}
+			if (isVerbose && input != null && input != "")
+			{
+				DebugUtil.Log("KeyView.InputString: <" + input + ">");
+			}
+			return input;
+		}
+
+		// Character type is more efficient, but string type is more compatible with most data.
+		public static List<string> InputList()
+		{
+			return DataUtil.Split(InputString(), "");
 		}
 	}
 }
