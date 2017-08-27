e13adac90cdfde856a6b83b77855d0caa1892128
Ethan Kennerly <ethan@lumoslabs.com>
2017-08-27 10:24:08 -0700

    Deck: Random integer in radius.

--- Deck.cs
+++ Deck.cs
@@ -1,68 +1,80 @@
 using UnityEngine;  // Mathf
-using System;  // Random
 using System.Collections.Generic;  // List
 
-public class Deck
+namespace Finegamedesign.Utils
 {
-	private static System.Random rng = new System.Random();
-
-	public static float Random()
+	public sealed class Deck
 	{
-		return (float) rng.NextDouble();
-	}
+		private static System.Random rng = new System.Random();
 
-	//
-	// Unity Random includes 1.0, which would be out of range.
-	// 
-	public static void ShuffleArray<T>(T[] deck)
-	{
-		for (int index = deck.Length - 1; 1 <= index; index--)
+		// Uniform distribution between -radius and +radius inclusive.
+		public static int RandomRadius(int radius)
 		{
-			int r = (int) Mathf.Floor(Random() * (index + 1));
-			T swap = deck[index];
-			deck[index] = deck[r];
-			deck[r] = swap;
+			return UnityEngine.Random.Range(-radius, radius + 1);
 		}
-	}
 
-	//
-	// Unity Random includes 1.0, which would be out of range.
-	// 
-	public static void ShuffleList<T>(List<T> deck)
-	{
-		for (int index = deck.Count - 1; 1 <= index; index--)
+		// Unity Random docs says it includes 1.0, which would be out of range.
+		public static float Random()
 		{
-			int r = (int) Mathf.Floor(Random() * (index + 1));
-			T swap = deck[index];
-			deck[index] = deck[r];
-			deck[r] = swap;
+			float value = UnityEngine.Random.value;
+			for (int attempt = 0; value >= 1.0f && attempt < 256; ++attempt)
+			{
+				value = UnityEngine.Random.value;
+			}
+			if (value >= 1.0f)
+			{
+				value = 0.0f;
+			}
+			return value;
 		}
-	}
 
-	private int index = -1;
-	private int length = -1;
-	private float[] cards;
-
-	public void Setup(float[] originals, int copies)
-	{
-		int original = originals.Length;
-		length = copies * original;
-		cards = new float[length];
-		for (index = 0; index < length; index++)
+		public static void ShuffleArray<T>(T[] deck)
 		{
-			cards[index] = originals[index % original];
+			for (int index = deck.Length - 1; 1 <= index; index--)
+			{
+				int r = (int) Mathf.Floor(Random() * (index + 1));
+				T swap = deck[index];
+				deck[index] = deck[r];
+				deck[r] = swap;
+			}
 		}
-		ShuffleArray(cards);
-		index = -1;
-	}
 
-	public float NextCard()
-	{
-		index++;
-		if (length <= index) {
+		public static void ShuffleList<T>(List<T> deck)
+		{
+			for (int index = deck.Count - 1; 1 <= index; index--)
+			{
+				int r = (int) Mathf.Floor(Random() * (index + 1));
+				T swap = deck[index];
+				deck[index] = deck[r];
+				deck[r] = swap;
+			}
+		}
+
+		private int index = -1;
+		private int length = -1;
+		private float[] cards;
+
+		public void Setup(float[] originals, int copies)
+		{
+			int original = originals.Length;
+			length = copies * original;
+			cards = new float[length];
+			for (index = 0; index < length; index++)
+			{
+				cards[index] = originals[index % original];
+			}
 			ShuffleArray(cards);
-			index = 0;
+			index = -1;
 		}
-		return cards[index];
+
+		public float NextCard()
+		{
+			index++;
+			if (length <= index) {
+				ShuffleArray(cards);
+				index = 0;
+			}
+			return cards[index];
+		}
 	}
 }
