95d43ec9fb4f272b40b332f853556f47ffd90715
Ethan Kennerly <ethan@lumoslabs.com>
2017-11-05 20:46:00 -0800

    Test Observable: Rename imitator class.

--- Editor/Tests/TestObservable.cs
+++ Editor/Tests/TestObservable.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 public sealed class TestObservable
 {
-	private sealed class Observer<T>
+	private sealed class Imitator<T>
 	{
 		private T m_Value;
 
@@ -24,55 +24,63 @@ public sealed class TestObservable
 	public void SetStringAndAppend()
 	{
 		Observable<string> state = new Observable<string>();
-		Observer<string> observer = new Observer<string>();
-		state.onChanged += observer.OnChanged;
+		Imitator<string> imitator = new Imitator<string>();
+		state.onChanged += imitator.OnChanged;
 		Assert.AreEqual(null, state.value);
-		Assert.AreEqual(null, observer.value);
+		Assert.AreEqual(null, imitator.value);
+
 		state.value = "none";
 		Assert.AreEqual("none", state.value);
-		Assert.AreEqual("none", observer.value);
+		Assert.AreEqual("none", imitator.value);
 		state.value = "active";
 		Assert.AreEqual("active", state.value);
-		Assert.AreEqual("active", observer.value);
+		Assert.AreEqual("active", imitator.value);
 
 		state.value += "_begin";
 		Assert.AreEqual("active_begin", state.value);
-		Assert.AreEqual("active_begin", observer.value);
+		Assert.AreEqual("active_begin", imitator.value);
+		state.onChanged -= imitator.OnChanged;
 	}
 
 	[Test]
 	public void SetIntAndAddAndDivide()
 	{
 		Observable<int> state = new Observable<int>();
-		Observer<int> observer = new Observer<int>();
-		state.onChanged += observer.OnChanged;
+		Imitator<int> imitator = new Imitator<int>();
+		state.onChanged += imitator.OnChanged;
 		Assert.AreEqual(0, state.value);
-		Assert.AreEqual(0, observer.value);
+		Assert.AreEqual(0, imitator.value);
+
 		state.value = 3;
 		Assert.AreEqual(3, state.value);
-		Assert.AreEqual(3, observer.value);
+		Assert.AreEqual(3, imitator.value);
+
 		state.value += 4;
 		Assert.AreEqual(7, state.value);
-		Assert.AreEqual(7, observer.value);
+		Assert.AreEqual(7, imitator.value);
 
 		state.value /= 3;
 		Assert.AreEqual(2, state.value);
-		Assert.AreEqual(2, observer.value);
+		Assert.AreEqual(2, imitator.value);
+		state.onChanged -= imitator.OnChanged;
 	}
 
 	[Test]
 	public void SetBoolAndXor()
 	{
 		Observable<bool> state = new Observable<bool>();
-		Observer<bool> observer = new Observer<bool>();
-		state.onChanged += observer.OnChanged;
+		Imitator<bool> imitator = new Imitator<bool>();
+		state.onChanged += imitator.OnChanged;
 		Assert.AreEqual(false, state.value);
-		Assert.AreEqual(false, observer.value);
+		Assert.AreEqual(false, imitator.value);
+
 		state.value = true;
 		Assert.AreEqual(true, state.value);
-		Assert.AreEqual(true, observer.value);
+		Assert.AreEqual(true, imitator.value);
+
 		state.value ^= true;
 		Assert.AreEqual(false, state.value);
-		Assert.AreEqual(false, observer.value);
+		Assert.AreEqual(false, imitator.value);
+		state.onChanged -= imitator.OnChanged;
 	}
 }
