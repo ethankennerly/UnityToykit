b805693b0b69c8eb68f3c8d91a6b24174a4b0230
Ethan Kennerly <ethan@lumoslabs.com>
2017-11-06 07:41:31 -0800

    Observable: Compares equal structs.

--- Observable.cs
+++ Observable.cs
@@ -1,24 +1,29 @@
 using System;
+using System.Collections.Generic;
 
+// Generically dispatches an event when the value changes.
+// Example: Editor/Tests/TestObservable.cs
+//
+// A tradeoff is the value is publicly exposed to changing.
 public sealed class Observable<T>
 {
 	public event Action<T> onChanged;
 
-	private T m_ObservedField;
+	private T m_Value;
 
 	public T value
 	{
 		get
 		{
-			return m_ObservedField;
+			return m_Value;
 		}
 		set
 		{
-			if (object.ReferenceEquals(m_ObservedField, value))
+			if (Equals(m_Value, value))
 			{
 				return;
 			}
-			m_ObservedField = value;
+			m_Value = value;
 			if (onChanged == null)
 			{
 				return;
@@ -27,20 +32,15 @@ public sealed class Observable<T>
 		}
 	}
 
+	// Check if equal when not sure if struct or class.
+	// https://stackoverflow.com/questions/390900/cant-operator-be-applied-to-generic-types-in-c
+	private static bool Equals(T x, T y)
+	{
+		return EqualityComparer<T>.Default.Equals(x, y);
+	}
+
 	public override string ToString()
 	{
-		return base.ToString() + "<" + value.ToString() + ">";
-	}
-
-	public static implicit operator Observable<T>(T value)
-	{
-		Observable<T> observable = new Observable<T>();
-		observable.value = value;
-		return observable;
-	}
-
-	public static implicit operator T(Observable<T> observable)
-	{
-		return observable.value;
+		return base.ToString() + "<" + m_Value.ToString() + ">";
 	}
 }
