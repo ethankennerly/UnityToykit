434208d9480c4996dbb033b1f42c539cf544ee24
Ethan Kennerly <ethankennerly@gmail.com>
2018-08-18 11:35:37 -0700

    Application Settings: WebGL ignores framerate.
    Otherwise there is a warning in the JavaScript console.

--- ApplicationSettings.cs
+++ ApplicationSettings.cs
@@ -7,9 +7,22 @@ namespace FineGameDesign.Utils
         [SerializeField]
         private int m_TargetFrameRate = 30;
 
-        public void OnEnable()
+        private void OnEnable()
         {
+            SetFrameRate();
+        }
+
+        /// <summary>
+        /// WebGL build ignores this.
+        /// Otherwise JavaScript console error:
+        ///     "Looks like you are rendering without using requestAnimationFrame for the main loop. You should use 0 for the frame rate in emscripten_set_main_loop in order to use requestAnimationFrame, as that can greatly improve your frame rates!"
+        /// <a href="https://forum.unity.com/threads/rendering-without-using-requestanimationframe-for-the-main-loop.373331/">Rendering without using requestAnimationFrame</a>
+        /// </summary>
+        private void SetFrameRate()
+        {
+            #if !UNITY_WEBGL
             Application.targetFrameRate = m_TargetFrameRate;
+            #endif
         }
     }
 }
