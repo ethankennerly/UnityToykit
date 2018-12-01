3ce3317fd192aca0c6e01b3c2fbeae911bec0d44
Ethan Kennerly <ethan.kennerly@glu.com>
2018-12-01 11:16:21 -0800

    Abstract Singleton: Query if instance exists.

--- ASingleton.cs
+++ ASingleton.cs
@@ -4,6 +4,14 @@ namespace FineGameDesign.Utils
     {
         private static T s_Instance;
 
+        /// <summary>
+        /// Useful to prevent accidentally recreating instance during teardown.
+        /// </summary>
+        public static bool InstanceExists()
+        {
+            return s_Instance != null;
+        }
+
         public static T instance
         {
             get
